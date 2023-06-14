a1c64b8ffc0e86c301089a15ffd53085f16e02e9
Ahmed Meftohu <91487090+MeftohuAhmed@users.noreply.github.com>
2023-06-14 13:05:36 -0400

    Update ATM.java
    Added class-level documentation

--- ATM.java
+++ ATM.java
@@ -1,13 +1,27 @@
 import java.io.IOException;
 
+/**
+ * The ATM class represents the entry point of the ATM project.
+ * It provides the main method to start the application and an introduction method to display project information.
+ */
 public class ATM {
 
+    /**
+     * The main method is the entry point of the ATM project.
+     * It creates an instance of the OptionMenu class and calls the displayMainMenu method to start the application.
+     *
+     * @param args Command-line arguments
+     */
     public static void main(String[] args) throws IOException {
         OptionMenu optionMenu = new OptionMenu();
         introduction();
         optionMenu.displayMainMenu();
     }
 
+    /**
+     * Displays an introduction message for the ATM project.
+     * Provides a brief description of the project and its functionality.
+     */
     public static void introduction() {
         System.out.println("=====================================");
         System.out.println("        WELCOME TO THE ATM PROJECT       ");
@@ -23,3 +37,4 @@ public class ATM {
         System.out.println("=====================================");
     }
 }
+
